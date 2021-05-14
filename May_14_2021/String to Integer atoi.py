6a8943ba2ce311ced36ec6705db33e445b33fd65
Dayanand Ghelaro <dayanandghelaro@gmail.com>
2021-05-14 18:52:34 +0500

    May 14, 2021 -- solved string to integer atoi -- Medium

---
+++ May_14_2021/String to Integer atoi.py
@@ -0,0 +1,42 @@
+class Solution:
+    """
+        Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+        The algorithm for myAtoi(string s) is as follows:
+
+        1. Read in and ignore any leading whitespace.
+        2. Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+        3. Read in next the characters until the next non-digit charcter or the end of the input is reached. The rest of the string is ignored.
+        4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+        5. If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+        6. Return the integer as the final result.
+    """
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        num = 0
+        if len(s)>0:
+            sign="+"
+            if s[0] in "+-":
+                sign = s[0]
+                s = s[1:]
+            for i in range(len(s)):
+                if s[i] not in "0123456789":
+                    s = s[:i]
+                    break
+                
+            if len(s)>0:
+                num = int(s)
+                if sign == "-":
+                    num = -1*num
+            if not ((-1*2**(31)) <= num <= (2**31 - 1)):
+                if sign == "-":
+                    num = -1*(2**31)
+                else:
+                    num = 2**31 -1
+        return num
+
+"""
+    Runtime: 28 ms
+    Faster than 93.05% of Python3 online submissions
+    Memory Usage: 14.2 MB less than 55.51% of Python3 online submissions
+"""
